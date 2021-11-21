d7610dd7b295e905ac719b3caf27d95184c72d60
miles <victoriayong@gmail.com>
2021-11-21 15:20:47 +0800

    Part 2: we tried

--- hmm.py
+++ hmm.py
@@ -31,7 +31,7 @@ def estimate_e(data):
     for y in total_count.keys():
         e[(y, unk_token)] = k / (total_count[y]+k)
 
-    return e, total_count.keys()
+    return e, list(total_count.keys())
 
 
 # Format dataset from file
@@ -90,37 +90,6 @@ def predict_label(filename, e, labels):
     inp.close()
 
 
-def evaluate_model(preds, gold):
-
-    preds_data = get_data(preds)
-    gold_data = get_data(gold)
-
-    correct_preds = 0
-    pred_count = 0
-    gold_count = 0
-
-    for sentence_index in range(len(preds_data)):
-
-        pred_count += len(preds_data[sentence_index])
-        gold_count += len(gold_data[sentence_index])
-
-        for record_index in range(len(preds_data[sentence_index])):
-            pred_record = preds_data[sentence_index][record_index]
-            gold_record = gold_data[sentence_index][record_index]
-
-            if pred_record == gold_record:
-                correct_preds += 1
-
-    print(correct_preds, pred_count, gold_count)
-    # total no of correctly predicted entities/total no of predicted entities
-    precision = correct_preds / pred_count
-    # total no of correctly predicted entities/total no of gold entities
-    recall = correct_preds / gold_count
-    F = 2/((1/precision)+(1/recall))
-    print(precision, recall)
-    return F
-
-
 def eval_script(preds, gold):
     gold = open(gold, "r", encoding='UTF-8')
     prediction = open(preds, "r", encoding='UTF-8')
@@ -129,7 +98,6 @@ def eval_script(preds, gold):
     return evalResult.compare_observed_to_predicted(observed, predicted)
 
 # Part 2
-# NOT DONE SOZ
 
 
 def estimate_q(data):
@@ -137,29 +105,108 @@ def estimate_q(data):
     q = {}
     transition_count = {}
     total_count = {}
+
     unk_token = '#UNK#'
 
     # iterate each sentence
     for idx in range(len(data)):
         sentence = data[idx]
-        for word in sentence:
-            yi, yi_1 = word[0], word[1]
 
-            transition_count[(y, x)] = transition_count.get((y, x), 0) + 1
-            total_count[y] = total_count.get(y, 0)+1
+        for word_index in range(len(sentence)+1):
+
+            if word_index < len(sentence):
+                yi = sentence[word_index][1]
+            else:
+                yi = "STOP"
+
+            yi_1 = sentence[word_index-1][1] if word_index-1 >= 0 else "START"
+            transition_count[(yi, yi_1)] = transition_count.get(
+                (yi, yi_1), 0)+1
+            total_count[yi_1] = total_count.get(yi_1, 0)+1
 
     for transition, count in transition_count.items():
-        e[transition] = count / (total_count[transition[0]] + k)
+        q[transition] = count/total_count[transition[1]]
+    return q
 
-    for y in total_count.keys():
-        e[(y, unk_token)] = k / (total_count[y]+k)
 
-    return e, total_count.keys()
+def viterbi(data, e, q, cat):
+    predictions = []  # array for best probabilities
+
+    for idx in range(len(data)):
+        sentence = data[idx]
+
+        pi = []
+        parents = []
+        for word_index in range(len(sentence)+1):
+            if word_index < len(sentence):
+                current_word = sentence[word_index]
+            else:
+                current_word = "STOP"
+
+            prev_word = sentence[word_index -
+                                 1] if word_index-1 >= 0 else "START"
+            print(f'Cur {current_word}, prev {prev_word}')
+            node_parent = []
+            node_value = []
+            for cat_index_current in range(len(cat)):
+                max_pi = 0
+                parent = None
+                for cat_index_prev in range(len(cat)):
+                    prev_pi = pi[word_index -
+                                 1][cat_index_prev] if word_index-1 >= 0 else 1
+                    #print("HERE", cat[cat_index_current], current_word)
+                    if current_word == "STOP":
+                        current_e = 1
+                    else:
+                        try:
+                            current_e = e[cat[cat_index_current], current_word]
+                        except KeyError:
+                            current_e = e[cat[cat_index_current], "#UNK#"]
+
+                    try:
+                        if current_word == "START":
+                            current_q = q[(cat[cat_index_current], "START")]
+                        elif current_word == "STOP":
+                            current_q = q[("STOP", cat[cat_index_prev])]
+                        else:
+                            current_q = q[(cat[cat_index_current],
+                                           cat[cat_index_prev])]
+                    except KeyError as error:
+                        # print(error)
+                        current_q = 0
+                    current_pi = prev_pi * current_q * current_e
+                    if current_pi > max_pi:
+                        parent = cat_index_prev
+                        max_pi = current_pi
+                node_value.append(max_pi)
+                node_parent.append(parent)
+            pi.append(node_value)
+            parents.append(node_parent)
+
+        prediction = []
+        for parent in reversed(parents):
+            if len(prediction) == 0:
+                prediction.append(parent[0])
+            else:
+                prediction.append(parent[prediction[-1]])
+
+        predictions.append(prediction)
+    return predictions
+
+
+def get_testing_data(path):
+    with open(path, encoding='utf-8') as f:
+        raw = f.read()
+        # array of sentences
+        sentences = raw.strip().split('\n\n')
+
+    clean = []
+    for sentence in sentences:
+        clean.append(sentence.split('\n'))
+    return clean
 
 
 # ============= START OF SCRIPT ===============
-
-
 if __name__ == '__main__':
 
     if len(sys.argv) < 2:
@@ -170,8 +217,20 @@ if __name__ == '__main__':
 
     # Part 1
     # Preparing Dataset
-    directory = f'{sys.argv[2]}/{sys.argv[2]}'
+    directory = f'{sys.argv[1]}/{sys.argv[1]}'
     data = get_data(f'{directory}/train')
     e, cat = estimate_e(data)
-    predict_label(f'{directory}/dev.in', e, cat)
-    print(eval_script(f'{directory}/dev.p1.out', f'{directory}/dev.out'))
+    print("cat", cat)
+
+    # predict_label(f'{directory}/dev.in', e, cat)
+    # print('TEST",eval_script(f'{directory}/dev.p1.out', f'{directory}/dev.out'))
+
+    q = estimate_q(data)
+
+    # dev = open(f'{directory}/dev.in', encoding='utf-8')
+    # dev_in = [line for line in dev.readlines()]
+    # print(dev_in)
+
+    test_data = get_testing_data(f'{directory}/dev.in')
+    predictions = viterbi(test_data, e, q, cat)
+    print(predictions)
